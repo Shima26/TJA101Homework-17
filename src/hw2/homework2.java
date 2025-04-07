9336d273d5f1e82fb858aeb5805bb33f966bc835
Grant Hung <a71526sea@gmail.com>
2025-04-07 19:17:23 +0800

    hw2-5

--- src/hw2/homework2.java
+++ src/hw2/homework2.java
@@ -1,13 +1,16 @@
 package hw2;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class homework2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 ///Q1//////////////////////////////////////////////
-		int i = 0;
+
 		int sum = 0;
-		for (i = 1; i <= 1000; i++) {
+		for (int i = 1; i <= 1000; i++) {
 			if (i % 2 == 1) {
 				continue;
 			} else {
@@ -17,9 +20,9 @@ public class homework2 {
 		System.out.println(sum);
 
 ///Q2//////////////////////////////////////////////
-		int j = 0;
+		
 		int sum2 = 1;
-		for (j = 1; j <= 10; j++) {
+		for (int j = 1; j <= 10; j++) {
 			sum2 = sum2 * j;
 
 		}
@@ -35,22 +38,43 @@ public class homework2 {
 		}
 		System.out.println(sum3);
 ///Q4//////////////////////////////////////////////			
-		int l = 1;
-		for (l=1;l<=10;l++) {
+		
+		for (int l=1;l<=10;l++) {
 			System.out.print((l*l)+" ");
 		}
+		System.out.println(" ");
+///Q5//////////////////////////////////////////////	
 		
+		int max = 49;
+		int counter =0;
+		List<Character> excludeNumbers = Arrays.asList('4');
+		for(int m=1;m<max ;m++) {
+		if (shouldExclude(m, excludeNumbers)) { continue; }	
+		System.out.print(m+" ");
+		counter = counter +1;
+		}
+		System.out.print("共"+counter+"個");
+	}
+	public static boolean shouldExclude(int number,List<Character> excludeNumbers) {
+		String numStr=String.valueOf(number);
+		for (char ch : excludeNumbers) {
+            if (numStr.indexOf(ch) >= 0) {
+                return true;  
+            }
+        }
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+        return false;
+        
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
-}
+
